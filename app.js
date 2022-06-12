31d58ab6b4dc0e6e4b0b8b6d64bf54b08a8c8826
FFerreira92 <F92ferreira@gmail.com>
2022-06-13 00:18:10 +0100

    making my website mobile compatible

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ $(document).ready(function(){
     dots:true,
     appendDots: '.slider-dot',
     dotsClass:'dots'
+  });
+
+  
+  let hamburger= document.querySelector('.hamburger-button');
+  let times=document.querySelector('.times');
+  let mobileNav =document.querySelector('.mobile-nav');
+
+  hamburger.addEventListener('click',function(){
+    mobileNav.classList.add('open');
   })
+
+  times.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+  })
+
 })
+
 
 const skillsSection = document.getElementById('skills-section');
 
