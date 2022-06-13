152583afe598d85ffd209177f123cf9ae66bce1b
FFerreira92 <F92ferreira@gmail.com>
2022-06-13 02:24:10 +0100

    core completed

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ $(document).ready(function(){
   let hamburger= document.querySelector('.hamburger-button');
   let times=document.querySelector('.times');
   let mobileNav =document.querySelector('.mobile-nav');
+  let click = document.querySelector('.click');
+  let click1 = document.querySelector('.click-1');
+  let click2 = document.querySelector('.click-2');
+  let click3 = document.querySelector('.click-3');
 
   hamburger.addEventListener('click',function(){
     mobileNav.classList.add('open');
@@ -19,6 +23,20 @@ $(document).ready(function(){
   times.addEventListener('click',function(){
     mobileNav.classList.remove('open');
   })
+
+  click.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+  })
+  click1.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+  })
+  click2.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+  })
+  click3.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+  })
+
 
 })
 
